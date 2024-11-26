a579ebd88bb19f9c6de26ce76a1861345d3a75ab
Florinda Hasani <florinda.hasani@valtech.com>
2024-11-26 14:51:50 +0100

    button ui changes and offices

--- app/src/main/java/com/example/booklibrary/data/book/repo/OfficeRepository.kt
+++ app/src/main/java/com/example/booklibrary/data/book/repo/OfficeRepository.kt
@@ -1,19 +1,31 @@
 package com.example.booklibrary.data.book.repo
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.example.booklibrary.data.book.models.ExceptionResponse
 import com.example.booklibrary.data.book.models.response.OfficeResponse
 import com.example.booklibrary.data.book.services.OfficeService
 import com.example.booklibrary.util.Resource
+import com.example.booklibrary.util.getUserJWTToken
 import com.google.gson.Gson
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import retrofit2.HttpException
 import javax.inject.Inject
 
 class OfficeRepository @Inject constructor(
-    private val officeService: OfficeService
+    private val officeService: OfficeService,
+    private val dataStore: DataStore<Preferences>
 ) {
+    private val token: String by lazy {
+        runBlocking {
+            val jwtToken = getUserJWTToken(dataStore).first() ?: ""
+            "Bearer $jwtToken"
+        }
+    }
     suspend fun getAllOffices(): Resource<List<OfficeResponse>> {
         val response = try {
-            officeService.getAllOffices()
+            officeService.getAllOffices(token)
         } catch (httpException: HttpException) {
             val errorResponse = Gson().fromJson(
                 httpException.response()?.errorBody()?.string(),
--- app/src/main/java/com/example/booklibrary/data/book/services/OfficeService.kt
+++ app/src/main/java/com/example/booklibrary/data/book/services/OfficeService.kt
@@ -1,9 +1,13 @@
 package com.example.booklibrary.data.book.services
 
 import com.example.booklibrary.data.book.models.response.OfficeResponse
+import com.example.booklibrary.util.AUTHORIZATION
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface OfficeService {
-    @GET("office/getAll")
-    suspend fun getAllOffices(): List<OfficeResponse>
+    @GET("/offices")
+    suspend fun getAllOffices(
+        @Header(AUTHORIZATION) token: String,
+    ): List<OfficeResponse>
 }
